63e30fe534c08b9d57ae1653e97c9f199e9b194e
Nox <biel.bianchi2004@hotmail.com>
2022-09-04 21:59:01 -0300

    Added game descriptions.

--- javascript/games.js
+++ javascript/games.js
@@ -4,7 +4,7 @@ let i = 0;
 content[i] = {
     name: 'Untitled Space Game',
     image_name : 'untitled_shooter.webp',
-    description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc porta ex sed molestie maximus. Integer ut sapien quis leo gravida volutpat. Ut quis nisl magna.    Donec iaculis ipsum a facilisis sagittis',
+    description : 'The untitled shooter game is basically one of my previous games, Space Invaders 3.0, but I added a online multiplayer mode to it.<BR>I was really exited to try out the new GameMaker RollBack System at the time.',
     link : 'https://gx.games/games/nes7cw/untitled-space-shooter-game/',
 
     date : '08/05/2022',
@@ -15,7 +15,7 @@ i++;
 content[i] = {
     name: 'Roll Your Life',
     image_name : 'roll_your_life.png',
-    description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc porta ex sed molestie maximus. Integer ut sapien quis leo gravida volutpat. Ut quis nisl magna.    Donec iaculis ipsum a facilisis sagittis',
+    description : `The GMTK Jam 2022's theme was "Roll the Dice", so we created a noir style game about a die that is being forced to work at a casino. The game made it to 89th place overall and 19th place in creativity category. This is the greatest jam we participated yet and I am very proud we got so far! :D`,
     link : 'https://bob-the-game-dev.itch.io/roll-your-life',
 
     date : '07/17/2022',
@@ -26,7 +26,7 @@ i++;
 content[i] = {
     name: 'My Happy Planet',
     image_name : 'happy_planet.png',
-    description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc porta ex sed molestie maximus. Integer ut sapien quis leo gravida volutpat. Ut quis nisl magna.    Donec iaculis ipsum a facilisis sagittis',
+    description : `At the beginning of 2022 I got into college, so I had to start learning C# and unity. Our teachers asked us to create a game with the "enviroment friendly" theme, this game is my team's project.`,
     link : 'https://nox2004.itch.io/my-happy-planet',
 
     date : '05/31/2022',
@@ -37,7 +37,7 @@ i++;
 content[i] = {
     name: 'Hey Brother',
     image_name : 'hey_brother.png',
-    description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc porta ex sed molestie maximus. Integer ut sapien quis leo gravida volutpat. Ut quis nisl magna.    Donec iaculis ipsum a facilisis sagittis',
+    description : `Hey Brother is a collection of minigames centered around 2 brothers who keep arguing and fighting. The game was a submission to the 7th edition of game jaaj, and got 2nd place overall.`,
     link : 'https://bob-the-game-dev.itch.io/hey-brother',
 
     date : '01/31/2022',
@@ -48,7 +48,7 @@ i++;
 content[i] = {
     name: 'Drinking Water Midnight',
     image_name : 'drinking_water.webp',
-    description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc porta ex sed molestie maximus. Integer ut sapien quis leo gravida volutpat. Ut quis nisl magna.    Donec iaculis ipsum a facilisis sagittis',
+    description : `"Drinking Water Midnight" is a game I made to celebrate 2021 Halloween, and is a short "horrorish" game based on the fear I had of going to the kitchen to get water late at night`,
     link : 'https://gamejolt.com/games/meia_noite/660730',
 
     date : '11/04/2021',
@@ -59,7 +59,7 @@ i++;
 content[i] = {
     name: 'Space Invaders 3.0',
     image_name : 'space_invaders_3.webp',
-    description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc porta ex sed molestie maximus. Integer ut sapien quis leo gravida volutpat. Ut quis nisl magna.    Donec iaculis ipsum a facilisis sagittis',
+    description : `This is a remake of my previous game (Space Invaders 2.0) that I created as a birthday gift for my father. Since 2 years have passed I got to make something way better with new graphics, power ups, enemys, and a boss.`,
     link : 'https://gamejolt.com/games/space_invaders_3/607792',
 
     date : '04/16/2021',
@@ -70,7 +70,7 @@ i++;
 content[i] = {
     name: 'The Unknown',
     image_name : 'Unknown.png',
-    description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc porta ex sed molestie maximus. Integer ut sapien quis leo gravida volutpat. Ut quis nisl magna.    Donec iaculis ipsum a facilisis sagittis',
+    description : `The Unknown is a mistery plataform game / ARG that we made for edition 5 of game jaaj. The game revolves the mechanic of using your pet as a plataform.`,
     link : 'https://gamejolt.com/games/desconhecido/571638',
 
     date : '01/09/2021',
@@ -81,7 +81,7 @@ i++;
 content[i] = {
     name: 'Sinuca Attack (Console)',
     image_name : 'sinuca_attack_console.webp',
-    description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc porta ex sed molestie maximus. Integer ut sapien quis leo gravida volutpat. Ut quis nisl magna.    Donec iaculis ipsum a facilisis sagittis',
+    description : `We ended up selling Sinuca Attack (previous game) console rights to East Asia Soft, and they published it to PlayStation, Xbox and Switch. Me and Rai added some levels and visuals to this version.`,
     link : 'https://store.playstation.com/pt-br/product/UP2005-PPSA06054_00-SINUCA0000000000',
 
     date : '06/30/2020',
@@ -92,7 +92,7 @@ i++;
 content[i] = {
     name: 'Sinuca Attack (Jam Ver.)',
     image_name : 'Sinuca_Attack_Jam.webp',
-    description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc porta ex sed molestie maximus. Integer ut sapien quis leo gravida volutpat. Ut quis nisl magna.    Donec iaculis ipsum a facilisis sagittis',
+    description : `Sinuca attack was a plataform/puzzle game we created for corona jaaj, a big brazilian game jam. It was the game I had the most fun idealizing and designing. At the end we won the "technical quality" category!`,
     link : 'https://gamejolt.com/games/sinuca_attack/477646',
 
     date : '03/29/2020',
@@ -103,7 +103,7 @@ i++;
 content[i] = {
     name: 'Evolutron Revolution Rampage...',
     image_name : 'evolutron.webp',
-    description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc porta ex sed molestie maximus. Integer ut sapien quis leo gravida volutpat. Ut quis nisl magna.    Donec iaculis ipsum a facilisis sagittis',
+    description : 'Evolutron is a remake of a game a youtuber made in video. Since the game is a roguelike, this was the first time I messed around with procedural generation. This is also one of the first games I made with Rai!',
     link : 'https://gamejolt.com/games/EvolutronRevolution/513907',
 
     date : '02/15/2020',
@@ -114,7 +114,9 @@ i++;
 content[i] = {
     name: 'Space Invaders 2.0',
     image_name : 'Space_Invaders_2.png',
-    description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc porta ex sed molestie maximus. Integer ut sapien quis leo gravida volutpat. Ut quis nisl magna.    Donec iaculis ipsum a facilisis sagittis',
+    description : `I made this one at father's day as a gift for my dad. 
+    He loved Space Invaders as a child, so I tried to make a space shooter with enemies based in the SI aliens.<BR>
+    I actually never published this game anywhere before making this site.`,
     link : 'https://drive.google.com/uc?export=download&id=1N5OLF6JvYuG0GSExFafr3vIj2M0ghFKA',
 
     date : '08/11/2019',
@@ -125,7 +127,8 @@ i++;
 content[i] = {
     name: '31 Days After the End of The World',
     image_name : '31_days.webp',
-    description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc porta ex sed molestie maximus. Integer ut sapien quis leo gravida volutpat. Ut quis nisl magna.    Donec iaculis ipsum a facilisis sagittis',
+    description : `2019 I joined my first Jam, the Brasil greatest jam, Game Jaaj (edition 3). <BR>
+    So I made this little plataformer game where you are a mailman in a post-apocalyptic world. The game made it to the top 50 of the jam! :D`,
     link : 'https://gamejolt.com/games/31DaysAfterTheEndOfTheWorld/427822',
 
     date : '07/21/2019',
@@ -136,7 +139,8 @@ i++;
 content[i] = {
     name: 'Flappy Penguin Shooter',
     image_name : 'flappy_penguin.png',
-    description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc porta ex sed molestie maximus. Integer ut sapien quis leo gravida volutpat. Ut quis nisl magna.    Donec iaculis ipsum a facilisis sagittis',
+    description : `Flappy Penguin was the second game I made aswell as the first game I "finished". <BR>
+    It's basically a flappy bird game with shooter mechanics. I was new to GameMaker at the time, so I experimented a lot when making this one.`,
     link : 'https://drive.google.com/uc?export=download&id=1tn4hvCtLiw8Hjdx8sQjJpSRf5N4n0-cN',
 
     date : '03/04/2019',
